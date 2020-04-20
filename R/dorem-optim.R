c0de5466e99f10fbda11df5be72d97951fbca699
Mladen Jovanović <coach.mladen.jovanovic@gmail.com>
2020-04-20 21:37:41 +0200

    L-BFGS-B implementation done

--- R/dorem-optim.R
+++ R/dorem-optim.R
@@ -1,22 +1,28 @@
 # Function that is called for optimization
 dorem_optim <- function(par, predict_func, predictors, outcome, control) {
 
-  objective_func <- function(par, predict_func, predictors, outcome, weights, na.rm) {
-
+  objective_func <- function(par, predict_func, predictors, outcome, weights, loss_func, na.rm) {
+      # Get the model predictions
       pred <- predict_func(par, predictors)
 
-      MSE <- mean((weights * (pred - outcome))^2, na.rm = na.rm)
+      # Calculate the loss
+      loss <- loss_func(
+        obs = outcome,
+        pred = pred,
+        weights = weights,
+        na.rm = na.rm)
 
-      return(MSE)
+      return(loss)
   }
 
   # L-BFGS-B method
+  if (control$optim_method == "L-BFGS-B") {
   model <- optimx::optimx(
     par = par,
     fn = objective_func,
     method = control$optim_method,
-    #lower = lower_bounds,
-    #upper = upper_bounds,
+    lower = control$coefs_lower,
+    upper = control$coefs_upper,
     hessian = TRUE,
     control = list(
       trace = control$optim_trace,
@@ -29,17 +35,21 @@ dorem_optim <- function(par, predict_func, predictors, outcome, control) {
     predictors = predictors,
     outcome = outcome,
     weights = control$weights,
+    loss_func = control$loss_func,
     na.rm = control$na.rm
   )
 
   par <- stats::coef(model)
   loss_func_value <- model$value
+  }
+
   predicted <- predict_func(par, predictors)
 
   # Generate performance metrics
   performance = control$perf_func(
     obs = outcome,
-    pred = predicted
+    pred = predicted,
+    na.rm = control$na.rm
   )
 
   # List to be returned
